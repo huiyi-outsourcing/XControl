9d7b72c4d324ab5db439007bfe58c9c151a6d0e4
Huiyi Li <lihuiyi0406@gmail.com>
2015-03-22 20:16:55 +0800

    1. add another tabcontrol style for sharewindow. 2. finish sharewindow tabcontrol part.

---
+++ TestApp/ShareWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for ShareWindow.xaml
+    /// </summary>
+    public partial class ShareWindow : Window
+    {
+        public ShareWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
