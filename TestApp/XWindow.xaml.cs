653399e617e2db5cc59c4b112ff297fdc7a4696d
Huiyi Li <lihuiyi0406@gmail.com>
2015-03-22 12:50:26 +0800

    Finish XWindow Header

---
+++ TestApp/XWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TestApp
+{
+    /// <summary>
+    /// Interaction logic for XWindow.xaml
+    /// </summary>
+    public partial class XWindow : Window
+    {
+        public XWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
